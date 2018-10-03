b717f71eadf142ffad2e374f9d588c9a4475c714
Dean Foster <Deanfoster45@gmail.com>
2018-10-03 17:23:10 -0400

    Add AppManager object, add asset bundle script, basic code for asset bundle loading

---
+++ phillips-ar/Assets/Scripts/AppManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class AppManager : MonoBehaviour {
+    private GameObject[] masks;
+
+	void Start () {
+        // Load the 3D mask models
+        // Would normally be called after image target reco by AR Core
+        masks = LoadBundle();
+
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    // Return loaded assets from the bundle
+    GameObject[] LoadBundle() {
+        // Load the asset bundle
+        AssetBundle bundle = 
+            AssetBundle.LoadFromFile(Path.Combine(Application.streamingAssetsPath, "luncheon"));
+
+        // Collect the assets
+        if (bundle == null)
+        {
+            Debug.Log("Failed to load AssetBundle!");
+            return null;
+        }
+        return (UnityEngine.GameObject[])bundle.LoadAllAssets();
+    }
+}
