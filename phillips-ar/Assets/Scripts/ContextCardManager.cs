b488e76ceba144cd99417c98e170fe13688c6737
Dean Foster <Deanfoster45@gmail.com>
2018-11-24 17:04:11 -0500

    Fixed bugs with sprite/canvas ordering

--- phillips-ar/Assets/Scripts/ContextCardManager.cs
+++ phillips-ar/Assets/Scripts/ContextCardManager.cs
@@ -9,21 +9,4 @@ using UnityEngine.UI;
 public class ContextCardManager : MonoBehaviour {
     [HideInInspector]
     public Text pieceTitle, pieceCaption, pieceParagraph;
-
-    //private GameObject contentObject;
-
-    //private void Awake() {
-    //    // Setup references
-    //    Debug.Log("Awake");
-    //    contentObject =
-    //            transform
-    //                .GetChild(0)
-    //                .GetChild(0)
-    //                .GetChild(0)
-    //                .GetChild(0).gameObject;
-    //    pieceTitle = contentObject.transform.GetChild(1).GetComponent<Text>();
-    //    pieceCaption = contentObject.transform.GetChild(2).GetComponent<Text>();
-    //    pieceParagraph = contentObject.transform.GetChild(3).GetComponent<Text>();
-    //    Debug.Log("Context references assigned");
-    //}
 }
